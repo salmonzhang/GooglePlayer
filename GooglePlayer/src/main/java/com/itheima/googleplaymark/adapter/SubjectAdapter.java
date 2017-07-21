0889cc9151b058149b0221572cc5b4ad107e0dd9
salmonzhang <ahzhangxuping@163.com>
2017-07-21 20:08:43 +0800

    专题界面适配器的抽取，将抽取的类放在SubjectViewHolder中

--- GooglePlayer/src/main/java/com/itheima/googleplaymark/adapter/SubjectAdapter.java
+++ GooglePlayer/src/main/java/com/itheima/googleplaymark/adapter/SubjectAdapter.java
@@ -1,22 +1,18 @@
 package com.itheima.googleplaymark.adapter;
 
-import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
-import android.widget.ImageView;
-import android.widget.TextView;
 
-import com.itheima.googleplaymark.R;
 import com.itheima.googleplaymark.bean.SubjectBean;
-import com.itheima.googleplaymark.utils.Utils;
+import com.itheima.googleplaymark.viewholder.SubjectViewHolder;
 
 import java.util.ArrayList;
 import java.util.List;
 
 /**
  * author:salmonzhang
- * Description:
+ * Description:专题界面的适配器
  * Date:2017/7/20 0020 00:29
  */
 
@@ -47,26 +43,12 @@ public class SubjectAdapter extends BaseAdapter {
     public View getView(int position, View convertView, ViewGroup parent) {
         SubjectViewHolder subjectViewHolder;
         if (convertView == null) {
-            convertView = LayoutInflater.from(parent.getContext()).inflate(R.layout.adapter_subject, parent, false);
             subjectViewHolder = new SubjectViewHolder();
-            subjectViewHolder.ivImage = (ImageView) convertView.findViewById(R.id.iv_image);
-            subjectViewHolder.tvDes = (TextView) convertView.findViewById(R.id.tv_des);
-            convertView.setTag(subjectViewHolder);
         } else {
             subjectViewHolder = (SubjectViewHolder) convertView.getTag();
         }
+        subjectViewHolder.bindView(mShowsItems.get(position));
 
-        //赋值
-        subjectViewHolder.tvDes.setText(mShowsItems.get(position).getDes());
-
-        //设置图片
-        Utils.SetFadeImage("http://127.0.0.1:8090/image?name="+mShowsItems.get(position).getUrl(),subjectViewHolder.ivImage);
-
-        return convertView;
-    }
-
-    class SubjectViewHolder{
-        public ImageView ivImage;
-        public TextView tvDes;
+        return subjectViewHolder.getView();
     }
 }
